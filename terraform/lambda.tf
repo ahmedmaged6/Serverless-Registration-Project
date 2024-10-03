49e4308a454b504433ac1c5a748bd26510b19cea
ahmedmaged6 <ahmeddmagedd957@gmail.com>
2024-10-03 19:32:32 +0300

    temporay commit

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,7 +18,41 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+data "aws_iam_policy_document" "lambda_logging_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:log-group:/aws/lambda/*:*"
+    ]
+  }
+}
 
+resource "aws_iam_policy" "lambda_logging_policy" {
+  name   = "lambda_logging_policy"
+  policy = data.aws_iam_policy_document.lambda_logging_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+}
+
+
+/*
+variable "api_paths" {
+  type    = list(string)
+  default = ["signup", "login", "profile", "confirm_code"]
+}
+
+  for_each    = toset(var.api_paths)
+
+  each.value
+*/
 # SignUp Lambda
 data "archive_file" "signup_lambda_zip_file" {
   type        = "zip"
@@ -35,7 +69,6 @@ resource "aws_lambda_function" "signup_lambda" {
   runtime = "python3.12"
   depends_on = [ aws_iam_role.iam_for_lambda ]
 }
-
 resource "aws_lambda_permission" "apigw_invoke" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -43,57 +76,3 @@ resource "aws_lambda_permission" "apigw_invoke" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.MyServerlessAPI.execution_arn}/*/*"
 }
-
-
-/*
-
-# ConfirmationCode Lambda
-data "archive_file" "confirmation_code_zip_file" {
-  type        = "zip"
-  source_file = "lambda/confirmation_code.py"
-  output_path = "lambda_zipped/confirmation_code_payload.zip"
-}
-
-resource "aws_lambda_function" "confirm_lambda" {
-  filename      = "${path.module}/lambda_zipped/confirmation_code_payload.zip"
-  function_name = "confirmation_code_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "confirmation_code.lambda_handler"
-  source_code_hash = data.archive_file.confirmation_code_zip_file.output_base64sha256
-  runtime = "python3.12"
-}
-
-
-# Login Lambda
-data "archive_file" "login_lambda_zip_file" {
-  type        = "zip"
-  source_file = "lambda/login.py"
-  output_path = "lambda_zipped/login_payload.zip"
-}
-
-resource "aws_lambda_function" "login_lambda" {
-  filename      = "${path.module}/lambda_zipped/login_payload.zip"
-  function_name = "login_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "login.lambda_handler"
-  source_code_hash = data.archive_file.login_lambda_zip_file.output_base64sha256
-  runtime = "python3.12"
-}
-
-# Profile Lambda
-data "archive_file" "profile_lambda_zip_file" {
-  type        = "zip"
-  source_file = "lambda/profile.py"
-  output_path = "lambda_zipped/profile_payload.zip"
-}
-
-resource "aws_lambda_function" "profile_lambda" {
-  filename      = "${path.module}/lambda_zipped/profile_payload.zip"
-  function_name = "profile_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "profile.lambda_handler"
-  source_code_hash = data.archive_file.profile_lambda_zip_file.output_base64sha256
-  runtime = "python3.12"
-}
-
-*/
